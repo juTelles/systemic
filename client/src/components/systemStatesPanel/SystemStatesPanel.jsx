e4e24932dde55457dca7a1f13c92db57810e91d7
JuTelles <julianatelles.contato@gmail.com>
2026-02-24 22:18:42 -0300

    feat(ui): add SystemStatePanel component

---
+++ client/src/components/systemStatesPanel/SystemStatesPanel.jsx
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-unused-vars
+// import react, { Children, useEffect, useState } from 'react';
+import SystemStateItem from '../systemStateItem/SystemStateItem';
+import styles from './SystemStatesPanel.module.css';
+
+function SystemStatesPanel() {
+  const ITEMS = [
+    { level: 'healthy', label: 'Saudável', active: true },
+    { level: 'warning', label: 'Alerta', active: false },
+    { level: 'critical', label: 'Crítico', active: false },
+  ];
+
+  return (
+    <div className={styles.systemStatesPanelWrapper}>
+      <h2 className={styles.painelTitle}>Estado do Sistema</h2>
+      <div className={styles.systemStateItemsContainer}>
+        {ITEMS.map((item) => (
+          <SystemStateItem
+            key={item.level}
+            level={item.level}
+            label={item.label}
+            active={item.active}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SystemStatesPanel;
