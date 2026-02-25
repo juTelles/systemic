5fd55213f6f37c1e61515f82f824ca47ee70e8a9
JuTelles <julianatelles.contato@gmail.com>
2026-02-24 22:11:22 -0300

    feat(ui): add DecisionPanel component

---
+++ client/src/components/decisionsPanel/DecisionsPanel.jsx
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-unused-vars
+// import react, { Children, useEffect, useState } from 'react';
+import DecisionOption from '../decisionButton/DecisionButton';
+import styles from './DecisionsPanel.module.css';
+
+function DecisionsPanel() {
+
+  const decisionsOptions = [
+    { optionDescription: 'Bug Local', cost: 10, type:'localBug', index: 1, categoryColor: '#FDE047' },
+    { optionDescription: 'Bug Estrutural', cost: 20, type:'strucuturalBug', index: 2, categoryColor: '#FF8A00' },
+    { optionDescription: 'Bug de Requisição', cost: 15, type:'requsitionsBug', index: 3, categoryColor: '#FF0055' },
+    { optionDescription: 'Desenvolver Testes', cost: 5, type: 'tests', index: 4, categoryColor: '#00FF9F' },
+    { optionDescription: 'Bug Local com Testes', cost: 10, type:'localBug', index: 7, categoryColor: '#FDE047' },
+    { optionDescription: 'Bug Estrutural com Testes', cost: 20, type:'strucuturalBug', index: 8, categoryColor: '#FF8A00'},
+    { optionDescription: 'Bug de Requisição com Testes', cost: 15, type:'requsitionsBug', index: 9, categoryColor: '#FF0055' },
+    { optionDescription: 'Doar / Guardar', type: 'giveOrHoldPoints', cost: 2, index: 5, categoryColor: '#22d3ee'},
+  ];
+
+  return (
+    <div className={styles.decisionsPanelWrapper}>
+      <div className={styles.decisionsContainer}>
+        {decisionsOptions.map((option) => (
+          <DecisionOption
+            key={option.index}
+            optionDescription={option.optionDescription}
+            cost={option.cost}
+            type={option.type}
+            categoryColor={option.categoryColor}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DecisionsPanel;
