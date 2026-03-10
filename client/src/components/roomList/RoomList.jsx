036eedfe526ff242d6f8b79613d2ed39761fee98
JuTelles <julianatelles.contato@gmail.com>
2026-03-10 03:04:06 -0300

    feat(frontend): add RoomList component

---
+++ client/src/components/roomList/RoomList.jsx
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+// import react, { Children, useEffect, useState } from 'react';
+import styles from './RoomList.module.css';
+import RoomListPlayers from '../roomListPlayers/RoomListPlayers';
+import RoomPlayerForm from '../roomPlayerForm/RoomPlayerForm';
+
+function RoomList({rooms}) {
+  return (
+    <div className={styles.roomListContainer}>
+      <h2 className={styles.roomListTitle}>Salas</h2>
+      <div className={styles.roomListWrapper}>
+        {rooms.length === 0 ? (
+          <p className={styles.noRoomsMessage}>Nenhuma sala disponível</p>
+        ) : (
+          rooms.map((room) => (
+            <div key={room.id} className={styles.roomItemWrapper}>
+              <div key={room.id} className={styles.roomItemHeader}>
+                <li className={styles.roomItem}>
+                  Sala {room.id} - {room.phase}
+                </li>
+              </div>
+              <RoomListPlayers roomId={room.id} room={room} />
+              {room.playersCount < 4 ? <RoomPlayerForm roomId={room.id} room={room} /> : <p className={styles.roomFullMessage}>Sala pronta para jogar</p>}
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default RoomList;
