5a23c670b36e6fc8a50716dd5ecebda25beec2b7
JuTelles <julianatelles.contato@gmail.com>
2026-03-10 02:54:46 -0300

    feat(frontend): add roomsApi.js

---
+++ client/src/api/roomsApi.js
@@ -0,0 +1,39 @@
+import { apiFetch } from "./apiClient";
+
+export async function joinRoom(roomId, nickname) {
+  return apiFetch(`/rooms/${roomId}/join`, {
+    method: "POST",
+    body: JSON.stringify({ nickname })
+  });
+}
+
+export async function leaveRoom(roomId, playerId) {
+  // console.log(`Leaving room ${roomId} with playerId ${playerId}`);
+  return apiFetch(`/rooms/${roomId}/leave`, {
+    method: "POST",
+    body: JSON.stringify({ playerId })
+  });
+}
+
+export async function getRoomList() {
+  const response = await apiFetch(`/rooms`);
+  return response?.roomsList || [];
+}
+
+export async function createRoom() {
+  return apiFetch(`/rooms/createRoom`, {
+    method: "POST",
+  });
+}
+
+export async function getRoomState(roomId, rev) {
+  const res = await fetch(`/api/rooms/${roomId}/state?rev=${rev}`);
+  return res.json();
+}
+
+export async function sendAction(roomId, playerId, action) {
+  return apiFetch(`/rooms/${roomId}/action`, {
+    method: "POST",
+    body: JSON.stringify({ playerId, action })
+  });
+}
