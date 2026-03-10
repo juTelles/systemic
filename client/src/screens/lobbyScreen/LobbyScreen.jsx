8116dd0476a6f9981dba4c2a11fbc0d596c9a24c
JuTelles <julianatelles.contato@gmail.com>
2026-03-10 03:02:52 -0300

    feat(frontend): add handleCreadRoom hook to LobbyScreem

--- client/src/screens/lobbyScreen/LobbyScreen.jsx
+++ client/src/screens/lobbyScreen/LobbyScreen.jsx
@@ -1,10 +1,26 @@
-// eslint-disable-next-line no-unused-vars
 import styles from './LobbyScreen.module.css';
 import Header from '../../components/header/Header';
+import { createRoom } from './../../api/roomsApi';
+import Button from '../../components/button/Button';
+import RoomList from '../../components/RoomList/RoomList';
+import { useRoomsPolling } from '../../hooks/useRoomsPolling';
+
 
 function LobbyScreen() {
 
   const { rooms } = useRoomsPolling(2000);
+
+  async function handleCreateRoom() {
+    console.log('handleCreateRoom called');
+    try {
+      const result = await createRoom();
+      console.log(result);
+    } catch (err) {
+      console.error('Failed to createRoom:', err);
+      console.error(err);
+    }
+  }
+
   return (
     <div className={styles.pageContainer}>
             {console.log('room', rooms)}
