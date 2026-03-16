5a6969dee6707847478db50740b6850eb9d7b449
JuTelles <julianatelles.contato@gmail.com>
2026-03-16 09:45:36 -0300

    feat(frontend): add set_ready button trigger to Players

--- client/src/components/player/Player.jsx
+++ client/src/components/player/Player.jsx
@@ -1,12 +1,41 @@
 // eslint-disable-next-line no-unused-vars
 import react from 'react';
 import styles from './Player.module.css';
+import Button from '../button/Button';
+import { BiCheck } from 'react-icons/bi';
 
-function Player({ playerName, playerPoints }) {
+function Player({
+  playerName,
+  playerStatus,
+  playerPoints,
+  isPreGame,
+  localPlayerId,
+  handleReady,
+}) {
   return (
     <div className={styles.playerItemWrapper}>
       <span className={styles.playerName}>{playerName}</span>
-      <span className={styles.playerPoints}>{playerPoints} </span>
+      {isPreGame ? (
+        <div className={styles.preGameStatusWrapper}>
+          <span className={`${styles.playerStatus} ${styles.preGameStatus}`}>
+            {playerStatus == 'READY' ? 'PRONTO' : 'Esperando'}
+          </span>
+          <Button
+            label={<BiCheck size={25} />}
+            width={'20%'}
+            height={'1.2rem'}
+            margin={'3px 0px 3px 7px'}
+            borderRadius={'0px'}
+            onClick={handleReady}
+            padding={'0'}
+            disabled={localPlayerId == true ? false : true}
+          >
+            <BiCheck size={25} />
+          </Button>
+        </div>
+      ) : (
+        <span className={styles.playerPoints}>{playerPoints} </span>
+      )}
     </div>
   );
 }
