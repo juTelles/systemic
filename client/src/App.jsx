7d5fb7a94b03951337af6ad7ff9a0b25b827df6e
JuTelles <julianatelles.contato@gmail.com>
2026-03-16 09:43:23 -0300

    feat(frontend): add localStorage on useState and change of screens to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,48 @@
 import styles from './App.module.css';
 import GameScreen from './screens/gameScreen/GameScreen';
 import './theme.css';
+import { useState } from 'react';
 import LobbyScreen from './screens/lobbyScreen/LobbyScreen';
 
+const STORAGE_KEYS = {
+  roomId: 'systemic_roomId',
+  localPlayerId: 'systemic_localPlayerId',
+};
+
 export default function App() {
+  const [roomId, setRoomId] = useState(
+    () => localStorage.getItem(STORAGE_KEYS.roomId) || ''
+  );
+  const [localPlayerId, setLocalPlayerId] = useState(
+    () => localStorage.getItem(STORAGE_KEYS.localPlayerId) || ''
+  );
+
+  const onJoinSuccess = (roomId, localPlayerId) => {
+    console.log(
+      `Player joined room successfully: roomId=${roomId}, localPlayerId=${localPlayerId}`
+    );
+    //TO DO: make custom hook to manage session and localStorage
+    localStorage.setItem(STORAGE_KEYS.roomId, roomId);
+    localStorage.setItem(STORAGE_KEYS.localPlayerId, localPlayerId);
+    setRoomId(roomId);
+    setLocalPlayerId(localPlayerId);
+  };
+
+  const clearSession = () => {
+    localStorage.removeItem(STORAGE_KEYS.roomId);
+    localStorage.removeItem(STORAGE_KEYS.localPlayerId);
+
+    setRoomId('');
+    setLocalPlayerId('');
+  };
 
   return (
     <div className={styles.app}>
-        <LobbyScreen/>
+      {!roomId ? (
+        <LobbyScreen onJoinSuccess={onJoinSuccess} />
+      ) : (
+        <GameScreen roomId={roomId} localPlayerId={localPlayerId} onSessionInvalid={clearSession}/>
+      )}
     </div>
   );
 }
