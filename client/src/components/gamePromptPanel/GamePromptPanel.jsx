de4b6c68974710040f1d0337a9de931588e17ccd
JuTelles <julianatelles.contato@gmail.com>
2026-03-16 09:47:40 -0300

    feat(frontend): add backend integration to GamePromptPanel

--- client/src/components/gamePromptPanel/GamePromptPanel.jsx
+++ client/src/components/gamePromptPanel/GamePromptPanel.jsx
@@ -1,13 +1,16 @@
 // eslint-disable-next-line no-unused-vars
-// import react, { Children, useEffect, useState } from 'react';
+import react, { Children, useEffect, useState } from 'react';
 import styles from './GamePromptPanel.module.css';
+import { instructions } from '../../../../shared/src/definitions/instructions';
 
-function GamePromptPanel({ text }) {
+function GamePromptPanel({ step }) {
   return (
     <div className={styles.gamePromptPanelWrapper}>
       <h2 className={styles.gamePromptPanelTitle}>Instruções</h2>
       <div className={styles.gamePromptPanel}>
-        <p className={styles.gamePromptPanelText}>{text}</p>
+        <p className={styles.gamePromptPanelText}>
+          {instructions()[step]?.description?.pt}
+        </p>
       </div>
     </div>
   );
