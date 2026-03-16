5662404de9b856db38dc4b1f5ad2084bcceb01d3
JuTelles <julianatelles.contato@gmail.com>
2026-03-16 09:52:53 -0300

    feat(frontend): add onJoinRoom and handleJoin function to Looby component

--- client/src/screens/lobbyScreen/LobbyScreen.jsx
+++ client/src/screens/lobbyScreen/LobbyScreen.jsx
@@ -1,40 +1,47 @@
 import styles from './LobbyScreen.module.css';
-import Header from '../../components/header/Header';
 import { createRoom } from './../../api/roomsApi';
 import Button from '../../components/button/Button';
 import RoomList from '../../components/RoomList/RoomList';
 import { useRoomsPolling } from '../../hooks/useRoomsPolling';
+import { joinRoom } from '../../api/roomsApi';
+import GameHeader from '../../components/gameHeader/GameHeader';
 
-
-function LobbyScreen() {
+function LobbyScreen({ onJoinSuccess }) {
 
   const { rooms } = useRoomsPolling(2000);
 
   async function handleCreateRoom() {
     console.log('handleCreateRoom called');
     try {
-      const result = await createRoom();
-      console.log(result);
+      createRoom();
     } catch (err) {
       console.error('Failed to createRoom:', err);
-      console.error(err);
     }
   }
 
+  const handleJoinRoom = (room, nickname) => {
+    const playerJoin = async () => {
+      try {
+        const result = await joinRoom(room.id, nickname);
+        if (result.ok) {
+          onJoinSuccess(room.id, result.playerId);
+        }
+        return result;
+      } catch (err) {
+        console.error('Failed to get room list:', err);
+      }
+    };
+    const result = playerJoin();
+    return result;
+  };
+
   return (
     <div className={styles.pageContainer}>
-            {console.log('room', rooms)}
-      <Header title="Systemic" />
+      {/* {console.log('room', rooms)} */}
+      <GameHeader title="Systemic" />
       <div className={styles.lobbyWrapper}>
-        <RoomList rooms={rooms}/>
+        <RoomList rooms={rooms} onJoinRoom={handleJoinRoom} />
         <div className={styles.buttonsWrapper}>
-          <Button
-            label={'Entrar na sala'}
-            reversed={true}
-            width="10rem"
-            height="4rem"
-            inverted={true}
-          />
           <Button
             label={'Criar uma sala'}
             width="10rem"
@@ -42,7 +49,9 @@ function LobbyScreen() {
             onClick={handleCreateRoom}
           />
         </div>
-        <span className={styles.bottomInfo}>Escolha um apelido com até 8 caracteres</span>
+        <span className={styles.bottomInfo}>
+          Escolha um apelido com até 8 caracteres
+        </span>
       </div>
     </div>
   );
