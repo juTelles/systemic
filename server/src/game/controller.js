fadb05f346d630dc22f69fcd32d482d6ae2e32f2
JuTelles <julianatelles.contato@gmail.com>
2026-03-16 07:14:15 -0300

    feat(backend): add createRunGameLoop controller

---
+++ server/src/game/controller.js
@@ -0,0 +1,45 @@
+export function createRunGameLoop({ rooms }) {
+  return function runGameLoop(
+    currentRoomState,
+    currentRoomId,
+    playerAction = null,
+    senderId = 'SYSTEM'
+  ) {
+    let iterations = 0;
+    const MAX_ITERATIONS = 10;
+    let state = structuredClone(currentRoomState);
+    const now = Date.now();
+
+    const currenteStepProgType = state.flow.step.progression?.trigger;
+
+    if (
+      playerAction &&
+      (!state.flow.blockedUntil || now >= state.flow.blockedUntil) &&
+      currenteStepProgType === 'PLAYER_INPUT'
+    ) {
+      state = rooms.applyRoomAction(currentRoomId, playerAction, senderId);
+      console.log('Applied player action:', playerAction, state);
+    }
+
+    while (iterations < MAX_ITERATIONS) {
+      if (state.flow.blockedUntil && now < state.flow.blockedUntil) break;
+
+      const autoAction = getActionForNextStep(state.flow.step);
+
+      if (!autoAction?.type) break;
+
+      state = rooms.applyRoomAction(currentRoomId, autoAction, senderId);
+      console.log('Applied auto action:', autoAction, state);
+      iterations++;
+    }
+    return state;
+  };
+}
+function getActionForNextStep(step) {
+  if (!step || !step.progression) return null;
+  if (step.progression.triggerNext === 'PLAYER_INPUT') {
+    return null;
+  } else {
+    return { type: step.next };
+  }
+}
