645457b3051f877aa9fd7809dfbedac008825a81
JuTelles <julianatelles.contato@gmail.com>
2026-03-16 08:52:38 -0300

    feat(frontend):  add decisionTxt file

---
+++ client/src/texts/decisionsTxt.js
@@ -0,0 +1,44 @@
+export const decisionsTxt = {
+  RESOLVE_LOCAL_BUG: {
+      label: {
+        pt: 'Bug Local',
+        en: 'Local Bug',
+      },
+      categoryColor: '#FDE047'
+    },
+    RESOLVE_STRUCTURAL_BUG: {
+      label: {
+        pt: 'Bug Estrutural',
+        en: 'Structural Bug',
+      },
+      categoryColor: '#FF8A00'
+    },
+    RESOLVE_REQUESTS_BUG: {
+      label: {
+        pt: 'Bug de Requisição',
+        en: 'Requests Bug',
+      },
+      categoryColor: '#FF0055'
+    },
+    DONATE_POINTS: {
+      label: {
+        pt: 'Doar Pontos',
+        en: 'Donate Points',
+      },
+      categoryColor: '#22d3ee'
+    },
+    HOLD_POINTS: {
+      label: {
+        pt: 'Guardar Pontos',
+        en: 'Hold Points',
+      },
+      categoryColor: '#22d3ee'
+    },
+    DEVELOP_TESTS: {
+      label: {
+        pt: 'Desenvolver Testes',
+        en: 'Develop Tests',
+      },
+      categoryColor: '#00FF9F'
+    },
+  }
