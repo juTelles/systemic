b61ad5f2418cc0cf0708d2e30c14353b224da2cd
JuTelles <julianatelles.contato@gmail.com>
2026-03-16 08:35:36 -0300

    feat(backend): add logic file for applyDecision logic

---
+++ server/src/game/decisions/logic.js
@@ -0,0 +1,54 @@
+import { decisions } from '../../../../shared/src/definitions/decisons.js';
+import { getPlayerObject } from '../selectors.js';
+
+export function getAvailableDecisions(decisionsObject, playerPoints) {
+  const decisionsAvailable = [];
+
+  for (const [decisionType, cost] of Object.entries(decisionsObject)) {
+    if (cost <= playerPoints) {
+      decisionsAvailable.push(decisionType);
+    }
+  }
+  return decisionsAvailable;
+}
+// TODO: Missing checking specifically for tests and component types
+
+export function applyDecisionEffect(action, state) {
+  let next = structuredClone(state);
+
+  const definition = decisions[action.chosen];
+  if (!definition) return next;
+
+  const context = resolveDecisionContext(action, next, definition);
+
+  const isValid = runDecisionValidators(definition.validators, context);
+  if (!isValid) return next;
+
+  const handler = decisionEffectHandlers[definition.effect];
+  if (!handler) return next;
+
+  return handler(next, context, definition);
+}
+
+function resolveDecisionContext(decisionAction, state) {
+  const decisionDefinition = decisions[decisionAction.chosen];
+  const currentPlayerId = state.flow.currentPlayerId;
+  const target = decisionAction.target;
+  const amount = decisionAction.selectedAmount;
+  const componentId = decisionAction.component;
+
+  const currentPlayer = getPlayerObject(currentPlayerId, state.players);
+
+  const componentObject = componentId
+    ? { ...state.components.nodes[componentId] }
+    : null;
+
+  return {
+    decisionDefinition,
+    currentPlayer,
+    target,
+    componentId,
+    componentObject,
+    amount
+  };
+}
