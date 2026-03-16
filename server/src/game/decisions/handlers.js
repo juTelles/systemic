b70d52d3980360a1223167a5f18fbf1c499ec9ce
JuTelles <julianatelles.contato@gmail.com>
2026-03-16 08:32:52 -0300

    feat(backend): add handlers file for applyDecision logic

---
+++ server/src/game/decisions/handlers.js
@@ -0,0 +1,59 @@
+import { addPointsToPlayerBankByDonation, addPointsToPlayerBankByHolding, resolveBug, applyTest, subtractPointsToPlayer, getPlayerObject } from "../gameHelpers";
+
+export const decisionHandlers = {
+  RESOLVE_BUG: handleResolveBugDecision,
+  DONATE_POINTS: handleDonatePointsDecision,
+  HOLD_POINTS: handleHoldPointsDecision,
+  DEVELOP_TESTS: handleDevelopTestsDecision,
+};
+
+export function handleResolveBugDecision({ next, currentPlayer, component, componentId, decisionDefinition }) {
+
+  if (!component) return next;
+
+  const updatedComponent = resolveBug(component);
+
+  const cost = next.gameConfig.decisionCosts[decisionDefinition.costType]
+  const updatedCurrentPlayer = subtractPointsToPlayer(currentPlayer, cost);
+
+  next.components.nodes[componentId] = updatedComponent;
+  next.players = next.players.map((player) => player.id === currentPlayer.id ? updatedCurrentPlayer : player);
+
+  return next;
+}
+
+export function handleDonatePointsDecision({ next, currentPlayer, amount, target }) {
+  const targetPlayer = getPlayerObject(target, next.players);
+  if (!targetPlayer) return next;
+
+  const updatedTargetPlayer = addPointsToPlayerBankByDonation(targetPlayer, amount, next.gameConfig.maxPlayerPoints);
+  const updatedCurrentPlayer = subtractPointsToPlayer(currentPlayer, amount);
+
+  next.players = next.players.map((player) => player.id === currentPlayer.id ? updatedCurrentPlayer : player);
+  next.players = next.players.map((player) => player.id === targetPlayer.id ? updatedTargetPlayer : player);
+
+  return next;
+}
+
+export function handleHoldPointsDecision({ next, currentPlayer, amount }) {
+
+  const updatedCurrentPlayer = addPointsToPlayerBankByHolding(currentPlayer, amount);
+
+  next.players = next.players.map((player) => player.id === currentPlayer.id ? updatedCurrentPlayer : player);
+
+  return next;
+}
+
+export function handleDevelopTestsDecision({ next, currentPlayer, component, componentId, decisionDefinition }) {
+  if (!component) return next;
+
+  const updatedComponent = applyTest(component);
+
+  const cost = next.gameConfig.decisionCosts[decisionDefinition.costType]
+  const updatedCurrentPlayer = subtractPointsToPlayer(currentPlayer, cost);
+
+  next.components.nodes[componentId] = updatedComponent;
+  next.players = next.players.map((player) => player.id === currentPlayer.id ? updatedCurrentPlayer : player);
+
+  return next;
+}
