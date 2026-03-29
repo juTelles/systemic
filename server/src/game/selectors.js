21a275789f2cd05e9855065bc4707bf9f9029088
JuTelles <julianatelles.contato@gmail.com>
2026-03-29 18:32:12 -0300

    fix(back): fix isGameReadyToStart to get runtime gameconfig and align with new transitionsResolver

--- server/src/game/selectors.js
+++ server/src/game/selectors.js
@@ -6,10 +6,10 @@ export  function getTotalPlayersPoints(player) {
   return player.handPoints + player.bankPoints;
 }
 
-export function isGameReadyToStart(gameConfig, currentState, desiredPhase, playerDesiredStatus) {
+export function isGameReadyToStart(currentState, desiredPhase, playerDesiredStatus) {
   const hasValidNumberOfPlayers =
-  currentState.players.length >= gameConfig.minPlayers &&
-  currentState.players.length <= gameConfig.maxPlayers;
+  currentState.players.length >= currentState.gameConfig.minPlayers &&
+  currentState.players.length <= currentState.gameConfig.maxPlayers;
   const isCorrectPhase = currentState.phase === desiredPhase;
   const allPlayersCorrectStatus = currentState.players.every(
     (player) => player.status === playerDesiredStatus
