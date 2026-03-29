cd3a64761aa570ef9e89ceeed1637aed3d78b8ff
JuTelles <julianatelles.contato@gmail.com>
2026-03-29 18:28:59 -0300

    refactor(back): update instructions definitions to align with new step names

--- shared/src/definitions/instructions.js
+++ shared/src/definitions/instructions.js
@@ -4,6 +4,13 @@ export const instructions = ({
   cardType,
   cardDescription,
 } = {}) => ({
+  WAITING_PLAYERS_READY: {
+    title: 'Ready Up',
+    description: {
+      pt: `Bem vindos ao Systemic! \nTodos os jogadores devem clicar no botão "Pronto" para iniciar o jogo! O jogo só começará quando todos estiverem prontos.`,
+      in: 'All players must click the "Ready" button to start the game! The game will only start when everyone is ready.',
+    },
+  },
   GAME_START: {
     title: 'instructions',
     description: {
@@ -11,35 +18,35 @@ export const instructions = ({
       in: "The game has started! The team's goal is to maintain and test all system components!",
     },
   },
-  TURN_START: {
+  ROUND_START: {
     title: 'Start of Turn',
     description: {
       pt: `Inicio do turno! O Time recebe ${points} Pontos de Tarefa a serem dividos igualmente entre os jogadores para gastar em Decisões Tecnicas.`,
       in: `Start of turn! The team receives ${points} "Task Points" to be divided equally among the players to spend on Technical Decisions.`,
     },
   },
-  PLAYER_TURN: {
+  TURN_START: {
     title: 'Player Turn',
     description: {
       pt: `É a vez do jogador ${playerNickname}!`,
       in: `It's ${playerNickname}'s turn! Spend your points on one of the possible decisions in the decision panel!`,
     },
   },
-  CHOOSE_DECISION: {
+  AWAIT_DECISION: {
     title: 'Valid Decisions',
     description: {
       pt: 'Você possui Pontos de Tarefa para gastar. Escolha uma decisão, pontos restantes que não forem guardados ou doados serão perdidos ao final do turno.',
       in: 'You still have Task Points to spend on available Technical Decisions. Choose a decision, remaining points will be lost at the end of the turn.',
     },
   },
-  DRAW_CARD: {
+  AWAIT_CARD_DRAW: {
     title: 'Draw Card',
     description: {
       pt: 'Click no deque para retirar uma carta! As cartas podem ser de 3 tipos: Pontos, Bugs ou Eventos. Cada carta tem um efeito diferente!',
       in: 'Click on the deck to draw a card! Cards can be of 3 types: Points, Bugs, or Events. Each card has a different effect!',
     },
   },
-  RESOLVE_CARD: {
+  PROCESSING_CARD: {
     title: 'Card Effects',
     description: {
       pt: `Você comprou uma carta de ${cardType}! ${cardDescription}`,
