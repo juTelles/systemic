bd471942dcdd4d44c5ee8aba56c8c00c8b87521a
JuTelles <julianatelles.contato@gmail.com>
2026-04-06 13:41:40 -0300

    refactor(front): change decisionTxt object to refactor decision buttons for better and dinamic UI info and

--- client/src/texts/decisionsTxt.js
+++ client/src/texts/decisionsTxt.js
@@ -1,44 +1,80 @@
-export const decisionsTxt = {
-  RESOLVE_LOCAL_BUG: {
-      label: {
-        pt: 'Bug Local',
-        en: 'Local Bug',
-      },
-      categoryColor: '#FDE047'
+export const decisionsTxt = (cost = 0) => ({
+  LOCAL: {
+    label: {
+      pt: `Bug Local\n`,
+      en: `Local Bug\n`,
     },
-    RESOLVE_STRUCTURAL_BUG: {
-      label: {
-        pt: 'Bug Estrutural',
-        en: 'Structural Bug',
-      },
-      categoryColor: '#FF8A00'
+    costLabel: {
+      pt: `${cost} Pontos`,
+      en: `${cost} Points`,
     },
-    RESOLVE_REQUESTS_BUG: {
-      label: {
-        pt: 'Bug de Requisição',
-        en: 'Requests Bug',
-      },
-      categoryColor: '#FF0055'
+    costTestedLabel: {
+      pt: `\nTestado: ${cost} Pontos`,
+      en: `\nTested: ${cost} Points`,
     },
-    DONATE_POINTS: {
-      label: {
-        pt: 'Doar Pontos',
-        en: 'Donate Points',
-      },
-      categoryColor: '#22d3ee'
+    categoryColor: '#FDE047',
+  },
+  STRUCTURAL: {
+    label: {
+      pt: `Bug Estrutural\n`,
+      en: `Structural Bug\n`,
     },
-    HOLD_POINTS: {
-      label: {
-        pt: 'Guardar Pontos',
-        en: 'Hold Points',
-      },
-      categoryColor: '#22d3ee'
+    costLabel: {
+      pt: `${cost} Pontos`,
+      en: `${cost} Points`,
     },
-    DEVELOP_TESTS: {
-      label: {
-        pt: 'Desenvolver Testes',
-        en: 'Develop Tests',
-      },
-      categoryColor: '#00FF9F'
+    costTestedLabel: {
+      pt: `\nTestado: ${cost} Pontos`,
+      en: `\nTested: ${cost} Points`,
     },
-  };
+    categoryColor: '#FF8A00',
+  },
+  REQUESTS: {
+    label: {
+      pt: `Bug de Requisições\n`,
+      en: `Requests Bug\n`,
+    },
+    costLabel: {
+      pt: `${cost} Pontos`,
+      en: `${cost} Points`,
+    },
+    costTestedLabel: {
+      pt: `\nTestado: ${cost} Pontos`,
+      en: `\nTested: ${cost} Points`,
+    },
+    categoryColor: '#FF0055',
+  },
+  DONATE_POINTS: {
+    label: {
+      pt: `Doar Pontos \n `,
+      en: `Donate Points \n Limit: ${cost} Points`,
+    },
+    costLabel: {
+      pt: `Limite: ${cost} Pontos`,
+      en: `${cost} Points`,
+    },
+    categoryColor: '#22d3ee',
+  },
+  HOLD_POINTS: {
+    label: {
+      pt: `Guardar Pontos\n`,
+      en: `Hold Points \n Limit: ${cost} Points`,
+    },
+    costLabel: {
+      pt: ` Limite: ${cost} Pontos`,
+      en: `${cost} Points`,
+    },
+    categoryColor: '#22d3ee',
+  },
+  TESTS: {
+    label: {
+      pt: `Desenvolver Testes\n`,
+      en: `Develop Tests\n`,
+    },
+    costLabel: {
+      pt: `${cost} Pontos`,
+      en: `${cost} Points`,
+    },
+    categoryColor: '#00FF9F',
+  },
+});
