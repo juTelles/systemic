f2de6900720ea8170add25fbeb3ba53ee4039918
JuTelles <julianatelles.contato@gmail.com>
2026-04-06 14:13:30 -0300

    feat(front): add targetMode to make players Player's line clickacable in PlayersPanel and to replace line with PlayersPointsForm when donate or hold decision is made and add dinamic text

--- client/src/components/player/Player.jsx
+++ client/src/components/player/Player.jsx
@@ -1,39 +1,33 @@
 // eslint-disable-next-line no-unused-vars
-import styles from './Player.module.css';
-import Button from '../button/Button';
-import { GiCheckMark } from "react-icons/gi";
+import styles from '../playersPanel/PlayersPanel.module.css';
 
 function Player({
+  id,
   playerName,
-  playerStatus,
-  playerPoints,
-  isPreGame,
-  localPlayerId,
-  handleReady,
+  pointsTotal,
+  pointsHand,
+  pointsBank,
+  targetMode,
+  handleSelectTargetPlayer,
 }) {
   return (
-    <div className={styles.playerItemWrapper}>
-      <span className={styles.playerName}>{playerName}</span>
-      {isPreGame ? (
-        <div className={styles.preGameStatusWrapper}>
-          <span className={`${styles.playerStatus} ${styles.preGameStatus}`}>
-            {playerStatus == 'READY' ? 'PRONTO' : 'Esperando'}
-          </span>
-          <Button
-            label={<GiCheckMark size={16}/>}
-            width={'20%'}
-            height={'1.2rem'}
-            margin={'3px 0px 3px 7px'}
-            borderRadius={'0px'}
-            onClick={handleReady}
-            padding={'0'}
-            disabled={localPlayerId == true ? false : true}
-          >
-          </Button>
-        </div>
-      ) : (
-        <span className={styles.playerPoints}>{playerPoints} </span>
-      )}
+    <div
+      className={`${styles.gridRow} ${styles.itemRow}`}
+      style={{ cursor: targetMode ? 'pointer' : 'default' }}
+      onClick={targetMode ? () => handleSelectTargetPlayer(id) : undefined}
+    >
+      <span className={`${styles.cell} ${styles.playerName}`}>
+        {playerName}
+      </span>
+      <span className={`${styles.cell} ${styles.playerPoints}`}>
+        {pointsHand}
+      </span>
+      <span className={`${styles.cell} ${styles.playerPoints}`}>
+        {pointsBank}
+      </span>
+      <span className={`${styles.cell} ${styles.playerPoints}`}>
+        {pointsTotal}
+      </span>
     </div>
   );
 }
--- client/src/components/playersPanel/PlayersPanel.jsx
+++ client/src/components/playersPanel/PlayersPanel.jsx
@@ -1,35 +1,75 @@
 // eslint-disable-next-line no-unused-vars
 import Player from '../player/Player';
+import PlayerPointsForm from '../playerPointsForm/PlayerPointsForm';
 import styles from './PlayersPanel.module.css';
-import { useRoomActions } from '../../actions/roomsActions';
+import { useState } from 'react';
 
-function PlayersPanel({ players, isPreGame, localPlayerId, roomId }) {
+function PlayersPanel({
+  players,
+  localPlayerId,
+  selectedDecisionUIId,
+  roomState,
+  isReadOnly,
+  txt,
+}) {
+  const currentPlayerId = roomState?.flow?.currentPlayerId;
+  const currentPlayer = selectedDecisionUIId == 'HOLD_POINTS' ? localPlayerId : null;
+  const [selectedTargetPlayerId, setSelectedTargetPlayerId] = useState(currentPlayer);
 
-  const { setReady } = useRoomActions(roomId, localPlayerId);
+  const targetMode = selectedDecisionUIId == 'DONATE_POINTS' ? true : false;
 
-  async function handleReady() {
-    const result = await setReady();
-    if (!result.ok) {
-      console.error('Error marking as ready:', result.error);
-    }
+  const handleSelectTargetPlayer = (player) => {
+    setSelectedTargetPlayerId(player);
+    console.log('Jogador selecionado:', player);
   };
 
-
   return (
-    <div className={styles.playersPanelWrapper}>
-      <h2 className={styles.playersPanelTitle}>Jogadores</h2>
-      <div className={styles.playersItemsContainer}>
-        {players?.map((player) => (
-          <Player
-            key={player.id}
-            playerName={player.nickname}
-            playerStatus={player.status}
-            playerPoints={player.handPoints + player.bankPoints}
-            isPreGame={isPreGame}
-            localPlayerId={player.id === localPlayerId}
-            handleReady={handleReady}
-          />
-        ))}
+    <div className={styles.playersPanelContainer}>
+      <div className={`${styles.gridRow} ${styles.playersPanelHeader}`}>
+        <h2 className={styles.playersPanelTitle}>{txt.playerTitle.pt}</h2>
+        <h2 className={styles.playersPanelTitle}>{txt.handPointsTitle.pt}</h2>
+        <h2 className={styles.playersPanelTitle}>{txt.bankPointsTitle.pt}</h2>
+        <h2 className={styles.playersPanelTitle}>{txt.totalPointsTitle.pt}</h2>
+      </div>
+
+      <div
+        className={styles.playersItemsContainer}
+        style={{ width: '100%', overflowY: 'auto' }}
+      >
+        {players?.map((player) => {
+          const inputMode =
+            !isReadOnly && selectedTargetPlayerId === player.id &&
+            (currentPlayer ||
+            (targetMode && player.id !== localPlayerId));
+
+          return inputMode ? (
+            <PlayerPointsForm
+              key={player.id}
+              id={player.id}
+              playerName={player.nickname}
+              pointsTotal={player.handPoints + player.bankPoints}
+              pointsHand={player.handPoints}
+              pointsBank={player.bankPoints}
+              maxPoints={roomState?.gameConfig?.taskPoints?.maxPlayerPoints}
+            />
+          ) : (
+            <Player
+              key={player.id}
+              id={player.id}
+              playerName={player.nickname}
+              pointsHand={player.handPoints}
+              pointsBank={player.bankPoints}
+              pointsTotal={player.handPoints + player.bankPoints}
+              localPlayerId={localPlayerId}
+              targetMode={targetMode}
+              currentPlayerId={currentPlayerId}
+              handleSelectTargetPlayer={handleSelectTargetPlayer}
+              selectedTargetPlayerId={selectedTargetPlayerId}
+              maxPoints={roomState?.gameConfig?.taskPoints?.maxPlayerPoints}
+              inputMode={inputMode}
+            />
+          );
+        })}
       </div>
     </div>
   );
