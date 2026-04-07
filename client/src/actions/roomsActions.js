7838716422f47abb09e7e78adaf5af63df6b4a28
JuTelles <julianatelles.contato@gmail.com>
2026-04-07 10:19:56 -0300

    feat(front): add action function for setDecisionChosen and setEndDecision

--- client/src/actions/roomsActions.js
+++ client/src/actions/roomsActions.js
@@ -2,12 +2,29 @@ import { sendAction } from '../api/roomsApi';
 import { ACTION_TYPES } from '../../../shared/src/constants/actionsTypes';
 
 export function useRoomActions(roomId, localPlayerId) {
-
   async function setReady() {
-    return sendAction(roomId, ACTION_TYPES.SET_READY, { senderId: localPlayerId });
+    return sendAction(roomId, ACTION_TYPES.SET_READY, {
+      senderId: localPlayerId,
+    });
+  }
+
+  async function setDecisonChosen(decision) {
+    return sendAction(roomId, ACTION_TYPES.APPLY_DECISION, {
+      senderId: localPlayerId,
+      chosen: decision.chosen,
+      target: decision.target,
+      selectedAmount: decision.selectedAmount,
+    });
+  }
+
+  async function setEndDecision() {
+    return sendAction(roomId, ACTION_TYPES.DRAW_CARD, {
+    });
   }
 
   return {
     setReady,
+    setDecisonChosen,
+    setEndDecision
   };
 }
