26465c02b4a774e4b30a4ba3e70e97bbdc0bf5ff
JuTelles <julianatelles.contato@gmail.com>
2026-04-07 10:37:55 -0300

    feat(back): add button to finalize action

--- client/src/components/lateralBar/LateralBar.jsx
+++ client/src/components/lateralBar/LateralBar.jsx
@@ -5,36 +5,57 @@ import CardBack from '../cardBack/CardBack';
 import { BsQuestionCircle } from "react-icons/bs";
 import { VscSignOut } from "react-icons/vsc";
 import { IoSettingsOutline } from "react-icons/io5";
+import Button from '../button/Button';
+import { useRoomActions } from '../../actions/roomsActions';
 
 function LateralBar({
   roomState,
   isPreGame,
   localPlayerId,
   roomId,
-  waitDecisionAmount,
-  waitDecisionTarget,
 }) {
+  const { setEndDecision } = useRoomActions(roomId, localPlayerId);
+
+  async function handleFinishDecision() {
+    const result = await setEndDecision();
+    if (!result.ok) {
+      console.error('Error finishing decision:', result.error);
+    }
+  }
 
   return (
     <div className={styles.lateralBar}>
       <div className={styles.menuContainer}>
         <button type="button" className={styles.helpButton}>
-        <BsQuestionCircle   size={43} className={styles.icon}/>
+          <BsQuestionCircle size={43} className={styles.icon} />
         </button>
         <button type="button" className={styles.menuButton}>
-        <IoSettingsOutline size={48} className={styles.icon}/>
+          <IoSettingsOutline size={48} className={styles.icon} />
         </button>
       </div>
       <div className={styles.cardContainer}>
-      <CardBack />
+        <CardBack />
       </div>
       <div className={styles.absorbedBugsPanelContainer}>
-      <AbsorbedBugsPanel />
+        <AbsorbedBugsPanel />
       </div>
-      <div className={styles.exitContainer}>
-        <button type="button" className={styles.exitButton}>
-        <VscSignOut  size={50} className={styles.icon}/>
-        </button>
+      <div className={styles.barBottomContainer}>
+        <div className={styles.finishDecisionContainer}>
+          <Button label="Finalizar Decisões"
+            width={'4rem'}
+            height={'4rem'}
+            borderRadius={'50px'}
+            inverted={true}
+            color={'var(--red)'}
+            fontSize={'var(--font-sm)'}
+            onClick={handleFinishDecision}
+          />
+        </div>
+        <div className={styles.exitContainer}>
+          <button type="button" className={styles.exitButton}>
+            <VscSignOut size={50} className={styles.icon} />
+          </button>
+        </div>
       </div>
     </div>
   );
