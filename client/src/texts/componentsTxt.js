24edf500d153be0662864c4f3b35e22159c0839b
JuTelles <julianatelles.contato@gmail.com>
2026-04-09 13:18:58 -0300

    feat(front): add text object for components labels

---
+++ client/src/texts/componentsTxt.js
@@ -0,0 +1,68 @@
+export const componentsTxt = {
+  applicationRequests: {
+    label: {
+      pt: 'Requisições de Aplicação',
+      en: 'Application Requests',
+    },
+  },
+  dataRequests: {
+    label: {
+      pt: 'Requisições de Dados',
+      en: 'Data Requests',
+    },
+  },
+  frontend: {
+    label: {
+      pt: 'Frontend',
+      en: 'Frontend',
+    },
+  },
+  backend: {
+    label: {
+      pt: 'Backend',
+      en: 'Backend',
+    },
+  },
+  database: {
+    label: {
+      pt: 'Banco de Dados',
+      en: 'Database',
+    },
+  },
+  interface: {
+    label: {
+      pt: 'Interface',
+      en: 'Interface',
+    },
+  },
+  interaction: {
+    label: {
+      pt: 'Interação',
+      en: 'Interaction',
+    },
+  },
+  logic: {
+    label: {
+      pt: 'Lógica',
+      en: 'Logic',
+    },
+  },
+  integrations: {
+    label: {
+      pt: 'Integrações',
+      en: 'Integrations',
+    },
+  },
+  data: {
+    label: {
+      pt: 'Dados',
+      en: 'Data',
+    },
+  },
+  structure: {
+    label: {
+      pt: 'Estrutura',
+      en: 'Structure',
+    },
+  },
+};
