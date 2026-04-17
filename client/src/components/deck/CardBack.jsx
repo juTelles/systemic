7830721e97c83c286ebd31dc1d35489b1bc213a3
JuTelles <julianatelles.contato@gmail.com>
2026-04-16 22:19:32 -0300

    feat(front): add CardBack component for displaying card back design

---
+++ client/src/components/deck/CardBack.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import styles from './Deck.module.css';
+
+function CardBack({ isFlipped, isFlipperBackCard }) {
+  return (
+    <div className={isFlipperBackCard ? styles.cardBackFlipper : styles.cardBackMain} data-status={isFlipped}>
+      <h2 className={styles.logoText}>Systemic</h2>
+      <span className={styles.cardIcon}>{'{#}'}</span>
+    </div>
+  );
+}
+
+export default CardBack;
