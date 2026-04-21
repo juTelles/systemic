b7fd12e48db471e0361519238fc0fe2c4c195589
JuTelles <julianatelles.contato@gmail.com>
2026-04-21 08:25:03 -0300

    feat(back): add health state instructions and instruction resolver function to add keys for instructions when the system health changes in the game

---
+++ server/src/game/systemHealthState/systemHealthInstructionResolver.js
@@ -0,0 +1,30 @@
+const HEALTH_STATE_INSTRUCTIONS = {
+  HEALTHY: {
+    WARNING: {
+      key: 'HEALTHY_TO_WARNING',
+    },
+    CRITICAL: {
+      key: 'HEALTHY_TO_CRITICAL',
+    },
+  },
+  WARNING: {
+    HEALTHY: {
+      key: 'WARNING_TO_HEALTHY',
+    },
+    CRITICAL: {
+      key: 'WARNING_TO_CRITICAL',
+    },
+  },
+  CRITICAL: {
+    HEALTHY: {
+      key: 'CRITICAL_TO_HEALTHY',
+    },
+    WARNING: {
+      key: 'CRITICAL_TO_WARNING',
+    },
+  },
+};
+
+export function getInstructionForHealthChange(oldHealthState, newHealthState) {
+  return HEALTH_STATE_INSTRUCTIONS[oldHealthState]?.[newHealthState].key;
+}
