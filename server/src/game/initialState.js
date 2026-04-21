44cc8b115dae3a68fe76744c4a50c05e1f52d4c0
JuTelles <julianatelles.contato@gmail.com>
2026-04-21 09:11:38 -0300

    feat(back): update initial state to include crisis round counter and modify player count for crisis rounds

--- server/src/game/initialState.js
+++ server/src/game/initialState.js
@@ -11,8 +11,10 @@ export function createInitialState({ roomId }) {
       currentPlayerId: null,
       isCrisisRound: false,
       blockedUntil: null,
+      crisisRoundCounter: 0,
       step: {
         name: 'WAITING_PLAYERS_READY',
+        stepInstructionKey: null,
         flowControl: {
           current: {
             accepts: 'PLAYER_INPUT',
@@ -57,7 +59,7 @@ export function createInitialState({ roomId }) {
         maxDonationPerPlayer: 2,
         maxHoldPerPlayer: 2,
         playerPerRound: 3,
-        playerPerCrisisRound: 4,
+        playerPerCrisisRound: 1,
       },
       decisionCosts: {
         RESOLVE_LOCAL_BUG: 2,
