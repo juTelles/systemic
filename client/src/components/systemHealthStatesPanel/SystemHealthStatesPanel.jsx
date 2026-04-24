e1d3ce136dfc968011d641e3d683ce8d5da7386e
JuTelles <julianatelles.contato@gmail.com>
2026-04-23 21:24:09 -0300

    feat(SystemHealthStatesPanel): enhance styling and functionality with crisis round indication and improved text effects for inhancen UX

--- client/src/components/systemHealthStatesPanel/SystemHealthStatesPanel.jsx
+++ client/src/components/systemHealthStatesPanel/SystemHealthStatesPanel.jsx
@@ -3,12 +3,16 @@ import SystemStateItem from './systemStateHealthItem/SystemStateHealthItem.jsx';
 import styles from './SystemHealthStatesPanel.module.css';
 import { SYSTEM_HEALTH_STATES as systemStates } from '../../../../shared/src/constants/gameEnums.js';
 
-function SystemHealthStatesPanel({ txt, systemHealth }) {
+function SystemHealthStatesPanel({ txt, systemHealth, crisisRound }) {
   const active = systemHealth;
 
   return (
-    <div className={styles.systemHealthStatesPanelWrapper}>
-      <h2 className={styles.systemHealthStatesPanelTitle}>{txt.systemStateTitle['pt']}</h2>
+    <div
+      className={`${styles.systemHealthStatesPanelWrapper} ${crisisRound ? styles.crisisRound : ''}`}
+    >
+      <h2 className={styles.systemHealthStatesPanelTitle}>
+        {txt.systemStateTitle['pt']}
+      </h2>
       <div className={styles.systemHealthStateItemsContainer}>
         {Object.values(systemStates).map((state) => (
           <SystemStateItem
