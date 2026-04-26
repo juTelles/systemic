f5a7cc834fea9e76a521e1862afdbfb56e59b802
JuTelles <julianatelles.contato@gmail.com>
2026-04-26 00:25:16 -0300

    feat(dropdown): add DropdownItem and DropdownRules components with styling and functionality

---
+++ client/src/components/dropdownRules/DropdownItem.jsx
@@ -0,0 +1,31 @@
+// eslint-disable-next-line no-unused-vars
+import styles from './DropdownRules.module.css';
+import Button from '../button/Button';
+import { useState } from 'react';
+
+
+
+function DropdownItem({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={styles.dropdownWrapper}>
+      <button
+        className={styles.dropdownHeader}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {title}
+        <span className={`${styles.arrow} ${isOpen ? styles.arrowRotated : ''}`}>
+          ▼
+        </span>
+      </button>
+      <div className={`${styles.dropdownContent} ${isOpen ? styles.dropdownOpen : ''}`}>
+           <div className={styles.dropdownContentInner}>
+        {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DropdownItem;
