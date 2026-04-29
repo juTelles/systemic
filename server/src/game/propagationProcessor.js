a620c2831ea07959af8dacb2b6786c719358fe47
JuTelles <julianatelles.contato@gmail.com>
2026-04-29 12:17:37 -0300

    fix(propagationProcessor): refactor request propagation logic to add new applyBugs and add log

--- server/src/game/propagationProcessor.js
+++ server/src/game/propagationProcessor.js
@@ -1,54 +1,58 @@
 import { getSaturatedNodesIdsByType, getNodesByIds } from './selectors.js';
-import { applyBug, cloneNodesForUpdate } from './gameHelpers.js';
+import { applyBugs } from './gameHelpers.js';
 import { createError } from '../utils/createErrors.js';
 import { ERRORS } from '../../../shared/src/constants/errors.js';
+import { addGameLog } from './gameLog.js';
 
-export function processEndRoundRequestPropagation(stateComponents) {
-  if (!stateComponents) {
+export function processEndRoundRequestPropagation(state) {
+  if (!state.components) {
     throw createError(ERRORS.INTERNAL_ERROR);
   }
 
   const saturatedRequestsIds = getSaturatedNodesIdsByType(
-    stateComponents,
+    state.components,
     'REQUESTS',
   );
+
+  if (saturatedRequestsIds.length === 0) {
+    return { propagated: false };
+  }
+
   const saturatedRequestNodes = getNodesByIds(
-    stateComponents,
+    state.components,
     saturatedRequestsIds,
   );
 
-  if (saturatedRequestNodes.length === 0) {
-    throw createError(ERRORS.INTERNAL_ERROR);
-  }
 
-  const updatedComponents = applyRequestPropagation(
-    stateComponents,
+  const { nodes, log } = applyRequestPropagation(
+    state.components,
     saturatedRequestNodes,
   );
-  console.info('[PROPAGATE_BUGS]', {
-      before: stateComponents.nodes,
-      after: updatedComponents.nodes,
-   });
-  return updatedComponents;
+
+  const gameLog = addGameLog(state, {
+    type: '[BUGS_APPLIED]',
+    reason: 'REQUESTS_PROPAGATION',
+    appliedBugs: log.bugsApplied,
+    amount: 1,
+    propagated: log.componentsPropagated,
+  });
+
+  return {
+    gameLog,
+    nodes: nodes,
+    propagated: true,
+  };
 }
 
 function applyRequestPropagation(stateComponents, saturatedRequestsNodes) {
-  const { updatedNodes, updatedComponents } =
-    cloneNodesForUpdate(stateComponents);
-
   const saturatedRequestChildren = saturatedRequestsNodes.flatMap(
     (node) => node.childrenIds,
   );
 
-  saturatedRequestChildren.forEach((componentId) => {
-    updatedNodes[componentId] = applyBug(
-      updatedNodes[componentId],
-      updatedComponents,
-    );
-  });
+  const { nodes, log } = applyBugs(saturatedRequestChildren, stateComponents.nodes);
 
   return {
-    ...updatedComponents,
-    nodes: updatedNodes,
+    log,
+    nodes: nodes,
   };
 }
